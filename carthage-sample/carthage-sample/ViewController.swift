978be511492a5f9846b2037fc25d77c6e6e03aa6
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-02-28 04:26:46 +0900

    initial commit

---
+++ carthage-sample/carthage-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carthage-sample
+//
+//  Created by 岩月海人 on 2019/02/28.
+//  Copyright © 2019 kaito2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ spm-sample/spm-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spm-sample
+//
+//  Created by 岩月海人 on 2019/02/27.
+//  Copyright © 2019 kaito2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
