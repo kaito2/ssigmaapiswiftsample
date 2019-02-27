f25454fd96e1052c0ba52957480821461b33d881
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-02-28 04:30:02 +0900

    initial commit

---
+++ ssigmaapiswiftsample/hello.swift
@@ -0,0 +1,13 @@
+//
+//  hello.swift
+//  ssigmaapiswiftsample
+//
+//  Created by 岩月海人 on 2019/02/28.
+//  Copyright © 2019 kaito2. All rights reserved.
+//
+
+import Foundation
+
+public func greet() {
+    print("hello")
+}
