79b9442a6386e4efcc877da8645c48864506f529
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-02-28 04:33:15 +0900

    add Carthage

---
+++ Carthage/Build/iOS/ssigmaapiswiftsample.framework/Headers/ssigmaapiswiftsample.h
@@ -0,0 +1,19 @@
+//
+//  ssigmaapiswiftsample.h
+//  ssigmaapiswiftsample
+//
+//  Created by 岩月海人 on 2019/02/28.
+//  Copyright © 2019 kaito2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ssigmaapiswiftsample.
+FOUNDATION_EXPORT double ssigmaapiswiftsampleVersionNumber;
+
+//! Project version string for ssigmaapiswiftsample.
+FOUNDATION_EXPORT const unsigned char ssigmaapiswiftsampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssigmaapiswiftsample/PublicHeader.h>
+
+
